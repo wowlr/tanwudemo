9ed658efda90ccb63ad00494887b22f0e780acd9
wowlr <381908861@qq.com>
2018-02-09 18:56:11 +0800

    集成单元测试日志系统

--- src/main/java/com/tanwuapp/tanwudemo/TanwudemoApplication.java
+++ src/main/java/com/tanwuapp/tanwudemo/TanwudemoApplication.java
@@ -2,19 +2,14 @@ package com.tanwuapp.tanwudemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.ComponentScan;
 
-@RestController
 @SpringBootApplication
+@ComponentScan
 public class TanwudemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TanwudemoApplication.class, args);
     }
 
-    @RequestMapping(value = "/",produces = "text/plain;charset=UTF-8")
-    public String index(){
-        return "Hello Spring Boo1111t!";
-    }
 }
---
+++ src/main/java/com/tanwuapp/tanwudemo/controller/UserController.java
@@ -0,0 +1,83 @@
+package com.tanwuapp.tanwudemo.controller;
+
+import com.tanwuapp.tanwudemo.domain.User;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserController {
+
+    static Map<Long, User> users = Collections.synchronizedMap(new HashMap<>());
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @ApiOperation(value="获取用户列表", notes="")
+    @RequestMapping(value="/", method=RequestMethod.GET)
+    public List<User> getUserList() {
+        // 处理"/users/"的GET请求，用来获取用户列表
+        // 还可以通过@RequestParam从页面中传递参数来进行查询条件或者翻页信息的传递
+        List<User> r = new ArrayList<>(users.values());
+        logger.info("Sample Debug Message1111");
+        logger.warn("Sample Trace Message2222");
+        return r;
+    }
+
+    @ApiOperation(value="创建用户", notes="根据User对象创建用户")
+    @RequestMapping(value="/", method=RequestMethod.POST)
+    public String postUser(@ModelAttribute User user) {
+        // 处理"/users/"的POST请求，用来创建User
+        // 除了@ModelAttribute绑定参数之外，还可以通过@RequestParam从页面中传递参数
+        users.put(user.getId(), user);
+        return "success";
+    }
+
+    @ApiOperation(value="获取用户详细信息", notes="根据url的id来获取用户详细信息")
+    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long")
+    @RequestMapping(value="/{id}", method=RequestMethod.GET)
+    public User getUser(@PathVariable Long id) {
+        // 处理"/users/{id}"的GET请求，用来获取url中id值的User信息
+        // url中的id可通过@PathVariable绑定到函数的参数中
+        return users.get(id);
+    }
+
+    @ApiOperation(value="更新用户详细信息", notes="根据url的id来指定更新对象，并根据传过来的user信息来更新用户详细信息")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long"),
+            @ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "User")
+    })
+    @RequestMapping(value="/{id}", method=RequestMethod.PUT)
+    public String putUser(@PathVariable Long id, @ModelAttribute User user) {
+        // 处理"/users/{id}"的PUT请求，用来更新User信息
+        User u = users.get(id);
+        u.setName(user.getName());
+        u.setAge(user.getAge());
+        users.put(id, u);
+
+        return "success";
+    }
+
+    @ApiOperation(value="删除用户", notes="根据url的id来指定删除对象")
+    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long")
+    @RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+    public String deleteUser(@PathVariable Long id) {
+        // 处理"/users/{id}"的DELETE请求，用来删除User
+        users.remove(id);
+        return "success";
+    }
+
+}
